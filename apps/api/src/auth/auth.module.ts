c12ddf0db2e9985a615839a3a1b71333c52538f3
vfurmane <vfurmane@student.42.fr>
2022-12-26 11:47:30 +0100

    feat(api): check the state of the request

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -2,12 +2,19 @@ import { HttpModule } from '@nestjs/axios';
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { FtOauth2Strategy } from './ft-oauth2.strategy';
+import { State } from './state.entity';
 
 @Module({
-  imports: [ConfigModule, HttpModule, PassportModule],
+  imports: [
+    ConfigModule,
+    HttpModule,
+    PassportModule,
+    TypeOrmModule.forFeature([State]),
+  ],
   providers: [AuthService, FtOauth2Strategy, Logger],
   controllers: [AuthController],
 })
