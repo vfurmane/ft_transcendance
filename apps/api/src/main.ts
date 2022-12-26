170866eb8cb381006d4e06c525c936f3abc09ebe
Valentin <valentin.furmanek@gmail.com>
2022-12-26 19:11:37 +0100

    docs(api): add Swagger OpenAPI documentation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('ft_transcendence')
+    .setDescription("The sapient's transcendence's API")
+    .setVersion('0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
