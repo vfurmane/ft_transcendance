1b1cfcd4b22dcc1c00da5fe9209709e5a833ff01
Valentin <valentin.furmanek@gmail.com>
2022-12-26 21:20:13 +0100

    feat(api): return error 401 when FtOauth2AuthGuard denies request

--- apps/api/src/auth/ft-oauth2-auth.guard.ts
+++ apps/api/src/auth/ft-oauth2-auth.guard.ts
@@ -1,5 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from 'types';
 
 @Injectable()
-export class FtOauth2AuthGuard extends AuthGuard('oauth2') {}
+export class FtOauth2AuthGuard extends AuthGuard('oauth2') {
+  handleRequest<TUser = User>(err: Error, user: TUser): TUser {
+    if (err || !user) {
+      throw new UnauthorizedException(err.message || 'Authentication failed');
+    }
+    return user;
+  }
+}
