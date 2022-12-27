c0c2fc2c0aad256cb30341f3a64c484b3e976c7a
Maxence Eudier <eudier.maxence@gmail.com>
2022-12-27 08:59:05 +0100

    front en cours

--- apps/web/pages/FriendEntity.tsx
+++ apps/web/pages/FriendEntity.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import Image from 'next/image';
 import Avatar from '../asset/Avatar.png';
+import Connect from '../asset/statusConnect.png'
 import RemoveFriend from '../asset/RemoveFriend.png';
 
 export default function FriendEntity () : JSX.Element {
     return (
-        <div className="cardContainer entity">
-            <div className="cardContainer">
-                <Image  alt='avatar' src={Avatar} width={50} height={50} className='avatar image' />
-                <div className="friendText">
-                    <h3>name</h3>
-                    <p>status</p>
+        <div className="shadowContainer">
+             <div className="cardContainer entity">
+                <div className="cardContainer">
+                    <Image  alt='avatar' src={Avatar} width={42} height={42} className='avatar image' />
+                    <Image alt='status' src={Connect} width={20} height={20} className='statusImage'/>
+                    <div className="friendText">
+                        <h3>name</h3>
+                        <p>status</p>
+                    </div>
                 </div>
+                <Image  alt='rm friend' src={RemoveFriend} width={20} height={20} className='image' />
             </div>
-            <Image  alt='rm friend' src={RemoveFriend} width={20} height={20} className='image' />
+            <div className="entityShadow"></div>
         </div>
     );
 }
