ea6c8efd7fb07464956cb05c557448c512a12585
Valentin <valentin.furmanek@gmail.com>
2022-12-27 13:11:22 +0100

    feat(api): add route to configure the TFA

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -9,6 +9,7 @@ import { UsersService } from 'src/users/users.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { FtOauth2Strategy } from './ft-oauth2.strategy';
+import { JwtStrategy } from './jwt.strategy';
 import { State } from './state.entity';
 import ftOauth2Configuration from '../config/ft-oauth2';
 
@@ -30,7 +31,7 @@ import ftOauth2Configuration from '../config/ft-oauth2';
     TypeOrmModule.forFeature([State]),
     UsersModule,
   ],
-  providers: [AuthService, FtOauth2Strategy, Logger, UsersService],
+  providers: [AuthService, FtOauth2Strategy, JwtStrategy, Logger, UsersService],
   controllers: [AuthController],
 })
 export class AuthModule {}
