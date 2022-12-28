33fcd52a40fd95a310cd5c434413fedb46741aff
Maxence Eudier <eudier.maxence@gmail.com>
2022-12-28 13:43:26 +0100

    work in progress

--- apps/web/pages/FriendEntity.tsx
+++ apps/web/pages/FriendEntity.tsx
@@ -7,7 +7,7 @@ import RemoveFriend from '../asset/RemoveFriend.png';
 export default function FriendEntity (props : {name : string, status: string}) : JSX.Element {
     return (
         <div className="shadowContainer">
-             <div className="cardContainer entity">
+            <div className="cardContainer entity">
                 <div className="cardContainer">
                     <Image  alt='avatar' src={Avatar} width={42} height={42} className='avatar L' />
                     <Image alt='status' src={Connect} width={20} height={20} className='statusImage'/>
@@ -18,7 +18,9 @@ export default function FriendEntity (props : {name : string, status: string}) :
                 </div>
                 <Image  alt='rm friend' src={RemoveFriend} width={20} height={20} className='L' />
             </div>
-            <div className="entityShadow"></div>
+            <div className="cardContainer entity entityShadow">
+            </div>
+           
         </div>
     );
 }
--- apps/web/pages/LeaderboardEntity.tsx
+++ apps/web/pages/LeaderboardEntity.tsx
@@ -4,11 +4,24 @@ import Avatar from '../asset/Avatar.png';
 import Connect from '../asset/statusConnect.png'
 
 export default function leaderboardEntity(props : {name : string, level: number, rank: number, status : string}) : JSX.Element {
+    
+    let div1 : JSX.Element;
+    let div2 : JSX.Element;
+
+    if (props.rank && Number(props.rank.toString().slice(-1)) <= 5 && Number(props.rank.toString().slice(-1)) != 0)
+    {
+        div1 = <div className="rank">{props.rank}</div>;
+        div2 = <div className="level">{props.level}</div>;
+    } else {
+        div2 = <div className="rank">{props.rank}</div>;
+        div1 = <div className="level">{props.level}</div>;
+    }
+
     return (
         <div className="leaderBoardContainer">
-            <div>{props.rank}</div>
-            <div className="shadowContainer">
-                <div className="cardContainer entity">
+            {div1}
+            <div className="shadowContainer ">
+                <div className="cardContainer entity small">
                     <div className="cardContainer">
                         <Image  alt='avatar' src={Avatar} width={42} height={42} className='avatar L' />
                         <Image alt='status' src={Connect} width={20} height={20} className='statusImage'/>
@@ -18,11 +31,12 @@ export default function leaderboardEntity(props : {name : string, level: number,
                         </div>
                     </div>
                 </div>
-                <div className="entityShadow"></div>
+                <div className="entityShadow small"></div>
                 
             </div>
-            <div>{props.level}</div>
+            {div2}
         </div>
         
     );
+    
 }
