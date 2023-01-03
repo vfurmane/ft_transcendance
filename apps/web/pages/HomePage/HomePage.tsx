fda46137490de4b899b4e60087feec5f4bcea65f
Maxence EUDIER <meudier@e2r8p6.clusters.42paris.fr>
2023-01-03 12:29:59 +0100

    responsive

--- apps/web/pages/HomePage/HomePage.tsx
+++ apps/web/pages/HomePage/HomePage.tsx
@@ -66,48 +66,50 @@ function Home() : JSX.Element {
     return (
         <div onClick={()=>close()} >
             <TopBar/>
-            <div className='container'> 
-                <div className='row'>
-                    <div className='col-10 offset-2 '>
-                        <h3 className='title'>Ft_Transcendence</h3>
+            <div className='illustration d-none d-lg-block'></div>
+            <div className='container '> 
+                    <div className='row'>
+                        <div className='col-10 offset-1'>
+                            <h3 className='title'>Ft_Transcendence</h3>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-3 offset-3 offset-lg-4 offset-xl-5'>
+                            <PlayButton handleClick={handleClickPlayButton} open={openPlayButton}/>
+                        </div>
+                        {openPlayButton ? 
+                        <div className='col-10 offset-1 offset-xl-0 offset-lg-1 col-lg-3 '>
+                            <div className='playMenuContainer'>
+                                <PlayMenu/>
+                            </div> 
+                        </div>
+                        : <></>}
+                    </div>
+                    <div className='row'>
+                        <div className='col-10 offset-1 col-lg-4'>
+                            <List title='Friends List' list={friendList} open={openFriendMenu} name={nameOfFriend} index={indexOfFriend}/>
+                        </div>
+                        <div className='col-10 offset-1  offset-lg-0 col-lg-6'>
+                            <List title='featuring' list={matchList} />
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-8 offset-2'>
+                            <h3 className='title small'>These guy are the best pong player of the world ... we are so pround of them !!</h3>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-10 offset-1'>
+                            <ArrayDoubleColumn title='leaderboard' list={leaderboard} openLeft={openFriendMenuLeaderBrdLeft} openRight={openFriendMenuLeaderBrdRight} name={nameOfFriend} index={indexOfFriend}/>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-4 offset-4'>
+                            <p className='textCenter'>Go back to top</p>
+                        </div>
                     </div>
                 </div>
-                <div className='row'>
-                    <div className='col-4 offset-4 '>
-                        <PlayButton handleClick={handleClickPlayButton} open={openPlayButton}/>
-                    </div>
-                    {openPlayButton ? 
-                    <div className='col-10 offset-1 col-lg-3'>
-                        <div className='playMenuContainer'>
-                            <PlayMenu/>
-                        </div> 
-                    </div>
-                    : <></>}
-                </div>
-                <div className='row'>
-                    <div className='col-10 offset-1 col-lg-4'>
-                         <List title='Friends List' list={friendList} open={openFriendMenu} name={nameOfFriend} index={indexOfFriend}/>
-                    </div>
-                    <div className='col-10 offset-1  offset-lg-0 col-lg-6'>
-                        <List title='featuring' list={matchList} />
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-8 offset-2'>
-                        <h3 className='title small'>These guy are the best pong player of the world ... we are so pround of them !!</h3>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-10 offset-1'>
-                        <ArrayDoubleColumn title='leaderboard' list={leaderboard} openLeft={openFriendMenuLeaderBrdLeft} openRight={openFriendMenuLeaderBrdRight} name={nameOfFriend} index={indexOfFriend}/>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-4 offset-4'>
-                        <p>Go back to top</p>
-                    </div>
-                </div>
-            </div>
+            
         </div>  
     );
 }
--- apps/web/pages/HomePage/PlayButton.tsx
+++ apps/web/pages/HomePage/PlayButton.tsx
@@ -3,7 +3,7 @@ import PlayMenu from './PlayMenu'
 
 export default function PlayButton(props : {handleClick: ()=>void, open: boolean}) : JSX.Element {   
     return (
-        <div className='shadowContainer MB' >
+        <div className='margePlayButton' >
             <button className='playButton' type='button' onClick={()=>props.handleClick()}>PLAY</button>
             <button className='playButtonShadow' type='button'/>
         </div>
