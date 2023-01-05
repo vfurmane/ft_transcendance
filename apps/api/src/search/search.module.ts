f36cc20283a336d0b4922d12094543beb86c57fd
eudier.maxence@icloud.com <eudier.maxence@icloud.com>
2023-01-05 10:57:51 +0100

    search init

---
+++ apps/api/src/search/search.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
+import { SearchController } from './search.controller'
+import { SearchService } from './search.service';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [SearchController],
+  providers: [SearchService],
+})
+export class SearchModule {}
