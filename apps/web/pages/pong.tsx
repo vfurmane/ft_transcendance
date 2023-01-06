3db8385d35b6209bc1b546c8dffa61b982d1af2d
Theodore NATON <tnaton@e3r12p8.clusters.42paris.fr>
2023-01-06 21:45:38 +0100

    changed a lot how everything is working, added Point, Vector, SAT for collision, Manual draw from point to allow complex polygone.

--- apps/web/pages/pong.tsx
+++ apps/web/pages/pong.tsx
@@ -69,7 +69,7 @@ class Board {
 		}
 		this.countUpdate++;
 		this.ball.update(this.player);
-		this.player.update();
+		this.player.update(this.ball);
 		this.cible.update(this.ball);
 		this.ball.draw(this.boardContext);
 		this.player.draw(this.boardContext);
@@ -84,6 +84,24 @@ class Board {
 	}
 }
 
+class Vector {
+	public	x;
+	public	y;
+
+	constructor(x:number,y:number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	product(other) {
+		return ((this.x * other.x) + (this.y * other.y));
+	}
+
+	perp() {
+		return (new Vector(-this.y, this.x));
+	}
+}
+
 class Point {
 	public	x;
 	public	y;
@@ -92,43 +110,127 @@ class Point {
 		this.x = x;
 		this.y = y;
 	}
+
+	vectorTo(other:Point) {
+		return (new Vector((other.x - this.x), (other.y - this.y)));
+	}
 }
 
 class Entity {
+	private	point:Point = [];
 	private	x;
 	private	y;
-	private	width;
-	private	height;
 	private	xspeed;
 	private	yspeed;
 
 	constructor(x:number,y:number,w:number,h:number) {
+		this.point[0] = new Point(x,y);
+		this.point[1] = new Point(x+w,y);
+		this.point[2] = new Point(x+w,y+h);
+		this.point[3] = new Point(x,y+h);
 		this.x = x;
 		this.y = y;
-		this.width = w;
-		this.height = h;
+	}
+
+	getx() {
+		return (this.point[0].x);
+	}
+
+	gety() {
+		return (this.point[0].y);
+	}
+
+	replaceTo(point:Point) {
+		let edges = this.getEdges();
+		this.point[0] = point;
+		for (let i = 0; i < this.point.length - 1; i++) {
+			this.point[i + 1].x = this.point[i].x + edges[i].x;
+			this.point[i + 1].y = this.point[i].y + edges[i].y;
+		}
+	}
+
+	moveTo(dir:Vector) {
+		for (let point of this.point) {
+			point.x += dir.x;
+			point.y += dir.y;
+		}
+	}
+
+	sat(other:Entity) {
+		let lines = this.getPerps().concat(other.getPerps());
+		let amax;
+		let amin;
+		let bmax;
+		let bmin;
+		let dot;
+
+		for (let line of lines) {
+			amax = null;
+			amin = null;
+			bmax = null;
+			bmin = null;
+			for (let point of this.point) {
+				dot = line.product(point);
+				if (amax === null || dot > amax) {
+					amax = dot;
+				}
+				if (amin === null || dot < amin) {
+					amin = dot;
+				}
+			}
+			for (let point of other.point) {
+				dot = line.product(point);
+				if (bmax === null || dot > bmax) {
+					bmax = dot;
+				}
+				if (bmin === null || dot < bmin) {
+					bmin = dot;
+				}
+			}
+			if ((amin < bmax && amin > bmin) || (bmin < amax && bmin > amin)) {
+				continue;
+			} else {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	draw(context){
-		context.fillStyle = "#fff";
-		context.fillRect(this.x,this.y,this.width,this.height);
+		context.beginPath();
+		context.moveTo(this.getx(), this.gety());
+		for (let point of this.point) {
+			context.lineTo(point.x, point.y);
+		}
+		context.closePath();
+		context.strokeStyle = 'white'
+		context.stroke();
+		context.fillStyle = 'white'
+		context.fill();
 	}
 
 	printPos(context) {
 		context.font = "14px sherif"
 		context.fillStyle = "#000";
-		context.fillText("x=" + this.x + " y=" + this.y + " x'=" + (this.x + this.width) + " y'=" + (this.y + this.height), this.x, this.y)
+		context.fillText("x=" + this.point[0].x + " y=" + this.point[0].y + " x'=" + (this.point[2].x) + " y'=" + (this.point[2].y), this.point[0].x, this.point[0].y)
 	}
 
-	collision(other:Entity) {
-		if ((other.x >= this.x + this.xspeed + this.width) || (other.x + other.width <= this.x + this.xspeed) || (other.y >= this.y + this.yspeed + this.height) || (other.y + other.height <= this.y + this.yspeed)) {
-			return (0);
-		} else if ((other.x >= this.x + this.width) || (other.x + other.width <= this.x)) {
-			return (Axe.X);
-		} else if ((other.y >= this.y + this.height) || (other.y + other.height <= this.y)) {
-			return (Axe.Y);
+	getEdges() {
+		let edges = [];
+		for (let i:number = 0; i < this.point.length - 1; i++) {
+			edges.push(this.point[i].vectorTo(this.point[i + 1]));
 		}
-		return (1);
+		edges.push(this.point.at(-1).vectorTo(this.point[0]));
+		return (edges);
+	}
+
+	getPerps() {
+		let edges = [];
+		for (let i:number = 0; i < this.point.length - 1; i++) {
+			edges.push((this.point[i].vectorTo(this.point[i + 1])).perp());
+		}
+		edges.push(this.point.at(-1).vectorTo(this.point[0]).perp());
+		return (edges);
 	}
 }
 
@@ -142,8 +244,16 @@ class Target extends Entity {
 	}
 
 	draw(context){
-		context.fillStyle = "#f00";
-		context.fillRect(this.x,this.y,this.width,this.height);
+		context.beginPath();
+		context.moveTo(this.getx(), this.gety());
+		for (let point of this.point) {
+			context.lineTo(point.x, point.y);
+		}
+		context.closePath();
+		context.strokeStyle = 'red'
+		context.stroke();
+		context.fillStyle = 'red'
+		context.fill();
 	}
 
 	randomVal(min, max) {
@@ -151,17 +261,15 @@ class Target extends Entity {
 	}
 
 	update(ball:Ball) {
-		if (this.collision(ball)) {
+		if (this.sat(ball)/*this.collision(ball)*/) {
 			Board.point++;
-			this.x = this.randomVal(window.innerWidth / 4, (window.innerWidth / 2 - 30))
-			this.y = this.randomVal(30, ((window.innerHeight / 2) - 30))
+			this.replaceTo(new Point(this.randomVal(window.innerWidth / 4, (window.innerWidth / 2 - 30)), this.randomVal(30, ((window.innerHeight / 2) - 30))));
 		}
 	}
 }
 
-
 class Ball extends Entity {
-	private	speed:number = 4;
+	private	speed:number = 1;
 
 	constructor(x:number,y:number,w:number,h:number) {
 		super(x,y,w,h);
@@ -170,33 +278,24 @@ class Ball extends Entity {
 	}
 
 	DontHitWallX() {
-		return (((this.x + this.xspeed) < ((window.innerWidth / 2) - this.width)) && ((this.x + this.xspeed) > 0))
+		return (((this.point[0].x + this.xspeed) < ((window.innerWidth / 2) - 40)) && ((this.point[0].x + this.xspeed) > 0))
 	}
 
 	DontHitWallY() {
-		return (((this.y + this.yspeed) < ((window.innerHeight / 2) - this.height)) && ((this.y + this.yspeed) > 0))
+		return (((this.point[0].y + this.yspeed) < ((window.innerHeight / 2) - 40)) && ((this.point[0].y + this.yspeed) > 0))
 	}
 
 	update(racket:Racket) {
-		let axe:number;
-		if (axe = this.collision(racket)) {
-			if (axe == Axe.X) {
-				this.xspeed = -this.xspeed;
-				if (this.xspeed > 0) {
-					this.xspeed += racket.speed;
-				} else {
-					this.xspeed -= racket.speed;
-				}
-			} else {
-				this.yspeed = -this.yspeed;
-				if (this.yspeed > 0) {
-					this.yspeed += racket.speed;
-				} else {
-					this.yspeed -= racket.speed;
-				}
-			}
+		if (this.sat(racket)) {
+			this.xspeed = -this.xspeed;
 			this.x += this.xspeed;
 			this.y += this.yspeed;
+			this.moveTo(new Vector(this.xspeed, this.yspeed));
+			if (this.xspeed > 0) {
+				this.xspeed += racket.speed;
+			} else {
+				this.xspeed -= racket.speed;
+			}
 		} else {
 			if (this.xspeed > 0) {
 				this.xspeed = this.speed;
@@ -210,8 +309,7 @@ class Ball extends Entity {
 			}
 			if (this.x + this.xspeed <= 0) {
 				Board.live--;
-				this.x = window.innerWidth / 4;
-				this.y = window.innerHeight / 4;
+				this.replaceTo(new Point(window.innerWidth / 4, window.innerHeight / 4));
 			}
 			if (this.DontHitWallX()) {
 				this.x += this.xspeed;
@@ -225,12 +323,13 @@ class Ball extends Entity {
 				this.yspeed = -this.yspeed;
 				this.y += this.yspeed;
 			}
+			this.moveTo(new Vector(this.xspeed, this.yspeed));
 		}
 	}
 }
 
 class Racket extends Entity {
-	private speed:number = 4;
+	private speed:number = 1;
 
 	constructor(x:number,y:number,w:number,h:number) {
 		super(x,y,w,h);
@@ -238,7 +337,7 @@ class Racket extends Entity {
 		this.yspeed = 0;
 	}
 
-	update() {
+	update(ball:Ball) {
 		if (Board.keyPressed[Key.UP] && Board.keyPressed[Key.DOWN]) {
 		} else 
 		if (Board.keyPressed[Key.UP]) {
@@ -246,11 +345,17 @@ class Racket extends Entity {
 		} else if (Board.keyPressed[Key.DOWN]) {
 			this.y += this.speed;
 		}
+		if (ball.y < this.y) {
+			this.y -= this.speed;
+		} else {
+			this.y += this.speed;
+		}
 		if (this.y < 0)
 			this.y = 0;
-		if (this.y + this.height > (window.innerHeight / 2)) {
-			this.y = ((window.innerHeight / 2) - this.height);
+		if (this.y + 100 > (window.innerHeight / 2)) {
+			this.y = ((window.innerHeight / 2) - 100);
 		}
+		this.replaceTo(new Point(this.x, this.y));
 	}
 }
 
@@ -259,13 +364,13 @@ const Canvas = props => {
 	let game = new Board();
 	function handleResize() {
 		game.updateBoard();
-		requestAnimationFrame(handleResize)
+		requestAnimationFrame(handleResize);
 	}
 
 	useEffect(() => {
 		game.init(canvasRef);
 		handleResize();
-		window.addEventListener('resize', handleResize)
+		window.addEventListener('resize', handleResize);
 	}, [])
 
 	return <canvas width='1500' height='1500' ref={canvasRef}/>
