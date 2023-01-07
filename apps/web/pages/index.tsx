b46ba76fce298dba74e01b19301c20835e136433
vfurmane <vfurmane@student.42.fr>
2023-01-07 15:02:40 +0100

    feat(web): redirect to different login or index page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,17 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Home from "./home";
 
 export default function Web(): JSX.Element {
+  const router = useRouter();
+
+  useEffect(() => {
+    const access_token = localStorage.getItem("access_token");
+    if (access_token === null) {
+      router.replace("/login");
+    }
+  }, [router]);
+
   return (
     <div>
       <Home />
