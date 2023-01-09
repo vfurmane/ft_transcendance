8b121dd397f1d4378e490696132f8948b0fc513e
vfurmane <vfurmane@student.42.fr>
2023-01-09 20:51:59 +0100

    refactor(api): remove duplicate User and State interfaces

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,16 +1,3 @@
-export interface State {
-  id: string;
-  created_at: Date;
-  updated_at: Date;
-  token: string;
-}
-
-export interface User {
-  id: string;
-  name: string;
-  email: string;
-}
-
 export interface FtUser {
   login: string;
   email: string;
