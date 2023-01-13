vfurmane/ft_transcendence

feat(api): add route that checks the OTP for login

apps/api/src/auth/auth.controller.ts
@@ -9,11 +9,13 @@ import {
   Logger,
   Post,
   Req,
+  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiBearerAuth,
+  ApiBody,
   ApiCreatedResponse,
   ApiMovedPermanentlyResponse,
   ApiNoContentResponse,
@@ -24,15 +26,17 @@ import {
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { UsersService } from '../users/users.service';
-import { AccessTokenResponse, SessionRequest } from 'types';
+import { AccessTokenResponse, SessionRequest, TfaNeededResponse } from 'types';
 import { AccessTokenResponseDto } from './access-token-response.dto';
 import { AuthService } from './auth.service';
+import { CheckTfaTokenStateDto } from './check-tfa-token-state.dto';
 import { CheckTfaTokenDto } from './check-tfa-token.dto';
 import { FtOauth2AuthGuard } from './ft-oauth2-auth.guard';
 import { FtOauth2Dto } from './ft-oauth2.dto';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { SpeakeasyGeneratedSecretDto } from './speakeasy-generated-secret.dto';
-import { StateGuard } from './state.guard';
+import { StateGetGuard } from './state-get.guard';
+import { StatePostGuard } from './state-post.guard';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -44,7 +48,7 @@ export class AuthController {
   ) {}
 
   @Get('/login/oauth2/42')
-  @UseGuards(StateGuard)
+  @UseGuards(StateGetGuard)
   @UseGuards(FtOauth2AuthGuard)
   @ApiOperation({
     summary:
@@ -60,12 +64,20 @@ export class AuthController {
     description:
       'The authentication failed (`code` or `state` may be invalid).',
   })
-  ftCallback(@Req() req: SessionRequest): AccessTokenResponse {
+  ftCallback(
+    @Req() req: SessionRequest,
+  ): AccessTokenResponse | TfaNeededResponse {
     if (!req.user) {
       this.logger.error(
         'This is the impossible type error where the user is authenticated but the `req.user` is `undefined`',
       );
       throw new InternalServerErrorException('Unexpected error');
+    }
+    if (req.user.tfa_setup) {
+      this.logger.log(
+        `${req.user.name} logged in using OAuth2, but TFA is needed`,
+      );
+      return { message: 'TFA needed' };
     }
     this.logger.log(`${req.user.name} logged in using OAuth2`);
     return this.authService.login(req.user);
@@ -134,13 +146,52 @@ export class AuthController {
   @ApiBadRequestResponse({
     description: 'The OTP is invalid or TFA is not setup yet.',
   })
-  checkTfa(@Req() req: SessionRequest, @Body() body: CheckTfaTokenDto): void {
+  async checkTfa(
+    @Req() req: SessionRequest,
+    @Body() body: CheckTfaTokenDto,
+  ): Promise<void> {
     if (!req.user) {
       this.logger.error(
         'This is the impossible type error where the user is authenticated but the `req.user` is `undefined`',
       );
       throw new InternalServerErrorException('Unexpected error');
     }
-    this.authService.checkTfa(req.user, body.token);
+    await this.authService.checkTfa(req.user, body.token);
+  }
+
+  @Post('tfa/login')
+  @HttpCode(200)
+  @UseGuards(StatePostGuard)
+  @ApiOperation({ summary: 'Login using an OTP token (as TFA).' })
+  @ApiBody({
+    type: CheckTfaTokenStateDto,
+  })
+  @ApiOkResponse({
+    description: 'The authentication succeeded.',
+    type: AccessTokenResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description:
+      'Authorization header is missing or invalid, or the first factor authentication has not been done yet.',
+  })
+  @ApiBadRequestResponse({
+    description: 'The OTP is invalid or TFA is not setup yet.',
+  })
+  async loginWithTfa(
+    @Req() req: SessionRequest,
+    @Body() body: CheckTfaTokenStateDto,
+  ): Promise<AccessTokenResponse> {
+    if (!req.state) {
+      this.logger.error(
+        'This is the impossible type error where the state is registered but the `req.state` is `undefined`',
+      );
+      throw new InternalServerErrorException('Unexpected error');
+    }
+    if (!req.state.user)
+      throw new UnauthorizedException('Missing first factor authentication.');
+    await this.authService.checkTfa(req.state.user, body.token);
+    await this.authService.removeState(req.state);
+    this.logger.log(`${req.state.user.name} validated TFA`);
+    return this.authService.login(req.state.user);
   }
 }

apps/api/src/auth/auth.service.ts
@@ -3,13 +3,21 @@ import { HttpService } from '@nestjs/axios';
 import { AxiosError } from 'axios';
 import { catchError, firstValueFrom, throwError } from 'rxjs';
 import * as speakeasy from 'speakeasy';
-import { AccessTokenResponse, FtUser, JwtPayload, User } from 'types';
+import { AccessTokenResponse, FtUser, JwtPayload, State, User } from 'types';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { State as StateEntity } from './state.entity';
+import { User as UserEntity } from '../users/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
   constructor(
+    @InjectRepository(StateEntity)
+    private readonly statesRepository: Repository<StateEntity>,
+    @InjectRepository(UserEntity)
+    private readonly usersRepository: Repository<UserEntity>,
     private readonly httpService: HttpService,
     private readonly jwtService: JwtService,
     private readonly logger: Logger,
@@ -47,7 +55,25 @@ export class AuthService {
     };
   }
 
-  checkTfa(user: User, token: string): boolean {
+  async getRequestState(stateToken: string, user: User): Promise<State> {
+    if (!stateToken) throw 'State parameter is needed.';
+
+    let state = await this.statesRepository.findOneBy({
+      token: stateToken,
+    });
+    if (state === null) {
+      state = new StateEntity();
+      state.token = stateToken;
+      if (user)
+        state.user = await this.usersRepository.findOneBy({
+          id: user.id,
+        });
+      await this.statesRepository.save(state);
+    }
+    return state;
+  }
+
+  async checkTfa(user: User, token: string): Promise<boolean> {
     if (user.tfa_secret === null)
       throw new BadRequestException('TFA not setup yet');
 
@@ -65,5 +91,10 @@ export class AuthService {
       this.usersService.validateTfa(user.id);
     }
     return true;
+  }
+
+  async removeState(state: State): Promise<void> {
+    state;
+    return;
   }
 }

vfurmane/ft_transcendance

apps/api/src/auth/auth.controller.ts
@@ -5,13 +5,16 @@ import {
   Delete,
   Get,
   HttpCode,
+  InternalServerErrorException,
   Logger,
   Post,
+  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiBearerAuth,
+  ApiBody,
   ApiCreatedResponse,
   ApiMovedPermanentlyResponse,
   ApiNoContentResponse,
@@ -21,18 +24,22 @@ import {
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
-import { AccessTokenResponse } from 'types';
+import { AccessTokenResponse, TfaNeededResponse } from 'types';
 import { UsersService } from '../users/users.service';
 import { AccessTokenResponseDto } from './access-token-response.dto';
 import { AuthService } from './auth.service';
+import { CheckTfaTokenStateDto } from './check-tfa-token-state.dto';
 import { CheckTfaTokenDto } from './check-tfa-token.dto';
 import { FtOauth2AuthGuard } from './ft-oauth2-auth.guard';
 import { FtOauth2Dto } from './ft-oauth2.dto';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { SpeakeasyGeneratedSecretDto } from './speakeasy-generated-secret.dto';
-import { StateGuard } from './state.guard';
 import { User as UserEntity } from '../users/user.entity';
 import { User } from '../common/decorators/user.decorator';
+import { StateGetGuard } from './state-get.guard';
+import { StatePostGuard } from './state-post.guard';
+import { State } from '../common/decorators/state.decorator';
+import { State as StateEntity } from './state.entity';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -44,7 +51,7 @@ export class AuthController {
   ) {}
 
   @Get('/login/oauth2/42')
-  @UseGuards(StateGuard)
+  @UseGuards(StateGetGuard)
   @UseGuards(FtOauth2AuthGuard)
   @ApiOperation({
     summary:
@@ -60,7 +67,13 @@ export class AuthController {
     description:
       'The authentication failed (`code` or `state` may be invalid).',
   })
-  ftCallback(@User() user: UserEntity): AccessTokenResponse {
+  ftCallback(
+    @User() user: UserEntity,
+  ): AccessTokenResponse | TfaNeededResponse {
+    if (user.tfa_setup) {
+      this.logger.log(`${user.name} logged in using OAuth2, but TFA is needed`);
+      return { message: 'TFA needed' };
+    }
     this.logger.log(`${user.name} logged in using OAuth2`);
     return this.authService.login(user);
   }
@@ -116,7 +129,46 @@ export class AuthController {
   @ApiBadRequestResponse({
     description: 'The OTP is invalid or TFA is not setup yet.',
   })
-  checkTfa(@User() user: UserEntity, @Body() body: CheckTfaTokenDto): void {
-    this.authService.checkTfa(user, body.token);
+  async checkTfa(
+    @User() user: UserEntity,
+    @Body() body: CheckTfaTokenDto,
+  ): Promise<void> {
+    await this.authService.checkTfa(user, body.token);
+  }
+
+  @Post('tfa/login')
+  @HttpCode(200)
+  @UseGuards(StatePostGuard)
+  @ApiOperation({ summary: 'Login using an OTP token (as TFA).' })
+  @ApiBody({
+    type: CheckTfaTokenStateDto,
+  })
+  @ApiOkResponse({
+    description: 'The authentication succeeded.',
+    type: AccessTokenResponseDto,
+  })
+  @ApiUnauthorizedResponse({
+    description:
+      'Authorization header is missing or invalid, or the first factor authentication has not been done yet.',
+  })
+  @ApiBadRequestResponse({
+    description: 'The OTP is invalid or TFA is not setup yet.',
+  })
+  async loginWithTfa(
+    @State() state: StateEntity,
+    @Body() body: CheckTfaTokenStateDto,
+  ): Promise<AccessTokenResponse> {
+    if (!state) {
+      this.logger.error(
+        'This is the impossible type error where the state is registered but the `req.state` is `undefined`',
+      );
+      throw new InternalServerErrorException('Unexpected error');
+    }
+    if (!state.user)
+      throw new UnauthorizedException('Missing first factor authentication.');
+    await this.authService.checkTfa(state.user, body.token);
+    await this.authService.removeState(state);
+    this.logger.log(`${state.user.name} validated TFA`);
+    return this.authService.login(state.user);
   }
 }

apps/api/src/auth/auth.service.ts
@@ -3,16 +3,23 @@ import { HttpService } from '@nestjs/axios';
 import { AxiosError } from 'axios';
 import { catchError, firstValueFrom, throwError } from 'rxjs';
 import { AccessTokenResponse, FtUser, JwtPayload } from 'types';
+import * as speakeasy from 'speakeasy';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { User } from '../users/user.entity';
-import * as speakeasy from 'speakeasy';
 import { UsersService } from '../users/users.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { State } from './state.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly configService: ConfigService,
+    @InjectRepository(State)
+    private readonly statesRepository: Repository<State>,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
     private readonly httpService: HttpService,
     private readonly jwtService: JwtService,
     private readonly logger: Logger,
@@ -53,7 +60,25 @@ export class AuthService {
     };
   }
 
-  checkTfa(user: User, token: string): boolean {
+  async getRequestState(stateToken: string, user: User): Promise<State> {
+    if (!stateToken) throw 'State parameter is needed.';
+
+    let state = await this.statesRepository.findOneBy({
+      token: stateToken,
+    });
+    if (state === null) {
+      state = new State();
+      state.token = stateToken;
+      if (user)
+        state.user = await this.usersRepository.findOneBy({
+          id: user.id,
+        });
+      await this.statesRepository.save(state);
+    }
+    return state;
+  }
+
+  async checkTfa(user: User, token: string): Promise<boolean> {
     if (user.tfa_secret === null)
       throw new BadRequestException('TFA not setup yet');
 
@@ -71,5 +96,10 @@ export class AuthService {
       this.usersService.validateTfa(user.id);
     }
     return true;
+  }
+
+  async removeState(state: State): Promise<void> {
+    state;
+    return;
   }
 }