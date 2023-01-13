vfurmane/ft_transcendence

feat(api): allow deletion of the TFA setup

apps/api/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import {
   BadRequestException,
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   InternalServerErrorException,
@@ -117,5 +118,20 @@ export class AuthController {
       throw new InternalServerErrorException('Unexpected error');
     }
     this.authService.checkTfa(req.user, body.token);
+  }
+
+  @Delete('tfa')
+  @HttpCode(204)
+  @UseGuards(JwtAuthGuard)
+  async removeTfa(@Req() req: SessionRequest): Promise<void> {
+    if (!req.user) {
+      this.logger.error(
+        'This is the impossible type error where the user is authenticated but the `req.user` is `undefined`',
+      );
+      throw new InternalServerErrorException('Unexpected error');
+    }
+    if (!req.user.tfa_setup)
+      throw new BadRequestException('TFA is not configured on your account');
+    this.usersService.removeTfa(req.user.id);
   }
 }

vfurmane/ft_transcendance

apps/api/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import {
   BadRequestException,
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   Logger,
@@ -99,5 +100,14 @@ export class AuthController {
   })
   checkTfa(@User() user: UserEntity, @Body() body: CheckTfaTokenDto): void {
     this.authService.checkTfa(user, body.token);
+  }
+
+  @Delete('tfa')
+  @HttpCode(204)
+  @UseGuards(JwtAuthGuard)
+  async removeTfa(@User() user: UserEntity): Promise<void> {
+    if (!user.tfa_setup)
+      throw new BadRequestException('TFA is not configured on your account');
+    this.usersService.removeTfa(user.id);
   }
 }