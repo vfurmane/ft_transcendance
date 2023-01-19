vfurmane/ft_transcendence

feat(api): add jti to JwtPayload and implement change_password

apps/api/src/users/users.service.ts
@@ -1,20 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { Repository, UpdateResult } from 'typeorm';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { In, Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as speakeasy from 'speakeasy';
-import { User } from 'types';
+import { AccessTokenResponse, User } from 'types';
 import { AddUserDto } from './add-user.dto';
 import { User as UserEntity } from './user.entity';
 import { SpeakeasyGeneratedSecretDto } from '../auth/speakeasy-generated-secret.dto';
+import * as bcrypt from 'bcrypt';
+import { ChangeUserPasswordDto } from './change-user-password.dto';
+import { Jwt as JwtEntity } from 'src/auth/jwt.entity';
+import { AuthService } from 'src/auth/auth.service';
 
 @Injectable()
 export class UsersService {
   constructor(
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
     @InjectRepository(UserEntity)
     private readonly usersRepository: Repository<UserEntity>,
+    @InjectRepository(JwtEntity)
+    private readonly jwtsRepository: Repository<JwtEntity>,
   ) {}
 
-  async getById(id: string): Promise<User | null> {
+  async getById(id: string): Promise<UserEntity | null> {
     return this.usersRepository.findOneBy({
       id,
     });
@@ -53,5 +61,32 @@ export class UsersService {
 
   async removeTfa(userId: string): Promise<UpdateResult> {
     return this.usersRepository.update({ id: userId }, { tfa_setup: false });
+  }
+
+  async changeUserPassword(
+    user: UserEntity,
+    changeUserPasswordDto: ChangeUserPasswordDto,
+  ): Promise<AccessTokenResponse> {
+    const salt = await bcrypt.genSalt();
+    changeUserPasswordDto.password = await bcrypt.hash(
+      changeUserPasswordDto.password,
+      salt,
+    );
+
+    await this.jwtsRepository
+      .find({
+        relations: ['user'],
+        loadRelationIds: true,
+        where: { user: In([user.id]) },
+      })
+      .then((jwts) => {
+        this.jwtsRepository.remove(jwts);
+      });
+
+    await this.usersRepository.update(
+      { id: user.id },
+      { password: changeUserPasswordDto.password },
+    );
+    return this.authService.login(user);
   }
 }

vfurmane/ft_transcendance

apps/api/src/users/users.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
-import { Repository, UpdateResult } from 'typeorm';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { In, Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import * as speakeasy from 'speakeasy';
-import { SpeakeasyGeneratedSecretDto } from 'src/auth/speakeasy-generated-secret.dto';
+import { SpeakeasyGeneratedSecretDto } from '../auth/speakeasy-generated-secret.dto';
+import { AccessTokenResponse } from 'types';
+import * as bcrypt from 'bcrypt';
+import { ChangeUserPasswordDto } from './change-user-password.dto';
+import { Jwt as JwtEntity } from '../auth/jwt.entity';
+import { AuthService } from '../auth/auth.service';
 
 export interface AddUserData {
   name: string;
@@ -16,6 +21,10 @@ export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+    @InjectRepository(JwtEntity)
+    private readonly jwtsRepository: Repository<JwtEntity>,
   ) {}
 
   async getById(id: string): Promise<User | null> {
@@ -63,5 +72,32 @@ export class UsersService {
 
   async removeTfa(userId: string): Promise<UpdateResult> {
     return this.usersRepository.update({ id: userId }, { tfa_setup: false });
+  }
+
+  async changeUserPassword(
+    user: User,
+    changeUserPasswordDto: ChangeUserPasswordDto,
+  ): Promise<AccessTokenResponse> {
+    const salt = await bcrypt.genSalt();
+    changeUserPasswordDto.password = await bcrypt.hash(
+      changeUserPasswordDto.password,
+      salt,
+    );
+
+    await this.jwtsRepository
+      .find({
+        relations: ['user'],
+        loadRelationIds: true,
+        where: { user: In([user.id]) },
+      })
+      .then((jwts) => {
+        this.jwtsRepository.remove(jwts);
+      });
+
+    await this.usersRepository.update(
+      { id: user.id },
+      { password: changeUserPasswordDto.password },
+    );
+    return this.authService.login(user);
   }
 }