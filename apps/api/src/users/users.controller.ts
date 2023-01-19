vfurmane/ft_transcendence

feat(api): route to return all users

apps/api/src/users/users.controller.ts
@@ -20,6 +20,13 @@ export class UsersController {
   ) {}
 
   @UseGuards(JwtAuthGuard)
+  //Choose an interceptor (which data to exclude)
+  @Get()
+  async findAll(): Promise<User[]> {
+    return await this.usersService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(ClassSerializerInterceptor)
   @Get('profile')
   async getProfile(@Request() req: SessionRequest): Promise<User> {

vfurmane/ft_transcendance

apps/api/src/users/users.controller.ts
@@ -19,6 +19,13 @@ export class UsersController {
   ) {}
 
   @UseGuards(JwtAuthGuard)
+  //Choose an interceptor (which data to exclude)
+  @Get()
+  async findAll(): Promise<UserEntity[]> {
+    return await this.usersService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(ClassSerializerInterceptor)
   @Get('profile')
   async getProfile(@User() user: UserEntity): Promise<UserEntity> {