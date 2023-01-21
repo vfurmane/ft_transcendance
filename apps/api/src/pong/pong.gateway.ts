5e27ca22ceb7918be840a47e1008e61edd41c173
tnaton <74002970+deblokc@users.noreply.github.com>
2023-01-21 18:38:25 +0100

    listen to event from player to launch game

--- apps/api/src/pong/pong.gateway.ts
+++ apps/api/src/pong/pong.gateway.ts
@@ -31,35 +31,63 @@ import { arrayBuffer } from "stream/consumers";
 			client.disconnect();
 			return;
 		  }
-		  client.data = { id: currentUser.sub, name: currentUser.name, isGaming: false };
+		  client.data = { id: currentUser.sub, name: currentUser.name, room: undefined, position: -1 };
 		  client.join(`user_${currentUser.sub}`);
 		  return 'Connection established';
 	}
 
-	@SubscribeMessage('startGame')
-	async  startGame(@ConnectedSocket() client : Socket)
-	{
+	@SubscribeMessage('move')
+	async	registerMove(@ConnectedSocket() client : Socket) {
 		const clientSockets = await this.server.in(`user_${client.data.id}`).fetchSockets();
+		const room = client.data.room;
+		if (room === undefined)
+			return 'You are not in a room'
+		if (this.room_id.indexOf(room) === -1) {
+			client.data.room = undefined
+			return 'Your room doesnt exist !'
+		}
+		if (client.data.position === -1) {
+			return 'You are not a player !'
+		}
+	}
+
+	@SubscribeMessage('startGame')
+	async  startGame(@ConnectedSocket() client : Socket) {
+		const clientSockets = await this.server.in(`user_${client.data.id}`).fetchSockets();
+		const room = client.data.room;
+		if (room === undefined)
+			return 'You are not in a room'
+		if (this.room_id.indexOf(room) === -1) {
+			client.data.room = undefined
+			return 'Your room doesnt exist !'
+		}
+		if (client.data.position === 0) {
+			let numberPlayer = (await this.server.in(room).fetchSockets()).length;
+			(await this.server.in(room).fetchSockets()).forEach(element => {
+				this.server.in(`user_${element.data.id}`).emit('startGame', { number_player: numberPlayer, position : element.data.position});
+			});
+			return 'Launching the game for room ' + room;
+		}
+		return 'You are not player 1 !'
 	}
 
 	@SubscribeMessage('searchGame')
-	async searchGame(@ConnectedSocket() client : Socket)
-	{
+	async searchGame(@ConnectedSocket() client : Socket) {
 		const clientSockets = await this.server.in(`user_${client.data.id}`).fetchSockets()
-		clientSockets.filter((socket) => socket.data.isGaming === true)
+		clientSockets.filter((socket) => socket.data.isGaming !== undefined)
 		if (clientSockets.length) return 'You are already in a room !'
-		this.server.in('game_room').emit("startGame", {})
 		if (this.room_id) {
 			for (let id of this.room_id) {
 				let count = (await this.server.in(id).fetchSockets()).length 
 				if (count < 5) {
 					client.join(id);
-					client.data.isGaming = true;
-					client.data.number = count;
+					client.data.room = id;
+					client.data.position = count;
 					if (count == 4) {
 						(await this.server.in(id).fetchSockets()).forEach(element => {
-							this.server.in(`user_${element.data.id}`).emit('startGame', { number_player: 5, position : element.data.number});
+							this.server.in(`user_${element.data.id}`).emit('startGame', { number_player: 5, position : element.data.position});
 						});
+						return 'Launched game for room ' + id;
 					}
 					return 'Joined room of id ' + id;
 				}
@@ -67,12 +95,12 @@ import { arrayBuffer } from "stream/consumers";
 			let num = (Number(this.room_id.at(-1)) + 1).toString();
 			this.room_id.push(num);
 			client.join(num);
-			client.data.isGaming = true;
+			client.data.room = num;
 			return 'Created room of id ' + num;
 		}
 		this.room_id = ['0'];
 		client.join('0');
-		client.data.isGaming = true;
+		client.data.room = '0';
 		return 'Created the first ever room !'
 	}
 
