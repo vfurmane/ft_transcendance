c77b98789e3d3af85b630c55f4c71a1b9c67c7a2
vfurmane <vfurmane@student.42.fr>
2023-02-01 14:51:10 +0100

    feat(api): emit users event types to the user's room

--- apps/api/src/app.gateway.ts
+++ apps/api/src/app.gateway.ts
@@ -1,11 +1,17 @@
-import { WebSocketGateway, WsException } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  WsException,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { CACHE_MANAGER, Inject, Logger } from '@nestjs/common';
 import { Cache } from 'cache-manager';
 import { AuthService } from './auth/auth.service';
 
 @WebSocketGateway()
 export class AppGateway {
+  @WebSocketServer() server!: Server;
+
   constructor(
     private readonly logger: Logger,
     @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
@@ -21,12 +27,18 @@ export class AppGateway {
     if (!currentUser) throw new WsException('Invalid token');
     client.data = { id: currentUser.sub, name: currentUser.name };
     client.join(`user_${currentUser.sub}`);
+    this.server
+      .to(`user_${currentUser.sub}`)
+      .emit(JSON.stringify({ event_type: 'connected' }));
     this.cacheManager.set(`user:${currentUser.sub}`, currentUser, 0);
     this.logger.log(`${currentUser.sub} connected`);
   }
 
   async handleDisconnect(client: Socket): Promise<void> {
     this.cacheManager.del(`user:${client.data.id}`);
+    this.server
+      .to(`user_${client.data.id}`)
+      .emit(JSON.stringify({ event_type: 'disconnected' }));
     this.logger.log(`${client.data.id} disconnected`);
   }
 }
