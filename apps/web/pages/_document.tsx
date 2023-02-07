5e61494877a18196d6a02bf5a323e58ca48639bd
vfurmane <vfurmane@student.42.fr>
2023-02-07 12:45:51 +0100

    refactor(web): import fonts inside document page

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -4,6 +4,16 @@ export default function Document(): JSX.Element {
   return (
     <Html>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lacquer&family=Press+Start+2P&family=Rubik+Vinyl&family=Saira&display=swap"
+          rel="stylesheet"
+        />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
           rel="stylesheet"
