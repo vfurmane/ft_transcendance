117811b92e554d0b6098c5e19cf85cbd58344c5d
vfurmane <vfurmane@student.42.fr>
2023-02-07 12:22:27 +0100

    fix(web): move stylesheet imports inside the document page

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -18,12 +18,6 @@ function App({ Component, ...rest }: AppProps): JSX.Element {
           @import
           url("https://fonts.googleapis.com/css2?family=Saira:wght@100&family=Press+Start+2P&family=Rubik+Vinyl&family=Lacquer&display=swap");
         </style>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
-          crossOrigin="anonymous"
-        ></link>
       </Head>
       <Provider store={store}>
         <Auth>
