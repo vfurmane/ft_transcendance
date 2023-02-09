97207b6b00ebc33b38cb6b7f58593a21ee9955db
vfurmane <vfurmane@student.42.fr>
2023-02-09 10:14:58 +0100

    feat(api): restrict user's ID to an UUID

--- apps/api/src/spied-user.dto.ts
+++ apps/api/src/spied-user.dto.ts
@@ -1,7 +1,7 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsUUID } from 'class-validator';
 
 export class SpiedUserDto {
-  @IsString()
+  @IsUUID()
   @IsNotEmpty()
   userId!: string;
 }
