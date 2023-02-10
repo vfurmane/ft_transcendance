175a7d4fcbfe79c94a83e68c30c15e11714fb30e
vfurmane <vfurmane@student.42.fr>
2023-02-10 15:11:56 +0100

    feat(web): redirect user to index page if they are not in queue

--- apps/web/pages/matchmaking.tsx
+++ apps/web/pages/matchmaking.tsx
@@ -1,11 +1,23 @@
+import { useRouter } from "next/router";
 import { ReactElement, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import styles from "styles/matchmaking-page.module.scss";
+import { Loading } from "../components/Loading";
 import { QueueReconnectionPrompt } from "../components/QueueReconnectionPrompt";
 import { useWebsocketContext } from "../components/Websocket";
+import { selectMatchmakingState } from "../store/MatchmakingSlice";
 
 export default function Matchmaking(): ReactElement {
   const [hasLeftQueue, setHasLeftQueue] = useState(false);
+  const [loading, setLoading] = useState(true);
   const websockets = useWebsocketContext();
+  const router = useRouter();
+  const MatchmakingState = useSelector(selectMatchmakingState);
+
+  useEffect(() => {
+    if (!MatchmakingState.isInQueue) router.push("/");
+    else setLoading(false);
+  }, [MatchmakingState.isInQueue, router]);
 
   useEffect(() => {
     return () => {
@@ -15,6 +27,9 @@ export default function Matchmaking(): ReactElement {
       }
     };
   }, [websockets.matchmaking]);
+
+  if (loading) return <Loading></Loading>;
+
   return (
     <div className={styles.container}>
       <h1>Waiting for an opponent...</h1>
