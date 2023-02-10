7da265dbe2a1923fdc35fd5d542b5c73de025313
vfurmane <vfurmane@student.42.fr>
2023-02-10 14:57:40 +0100

    fix(api): authenticate websocket connections

--- apps/api/src/matchmaking/matchmaking.gateway.ts
+++ apps/api/src/matchmaking/matchmaking.gateway.ts
@@ -8,6 +8,7 @@ import { Socket } from 'socket.io';
 import {
   ClassSerializerInterceptor,
   Logger,
+  UnauthorizedException,
   UseFilters,
   UseInterceptors,
   UsePipes,
@@ -17,6 +18,7 @@ import { MatchmakingService } from './matchmaking.service';
 import { JoinQueueDto } from './join-queue.dto';
 import { UsersService } from 'src/users/users.service';
 import { HttpExceptionTransformationFilter } from 'src/common/filters/HttpExceptionFilter.filter';
+import { AuthService } from 'src/auth/auth.service';
 
 @UseFilters(HttpExceptionTransformationFilter)
 @UsePipes(new ValidationPipe())
@@ -24,10 +26,21 @@ import { HttpExceptionTransformationFilter } from 'src/common/filters/HttpExcept
 @WebSocketGateway({ namespace: 'matchmaking' })
 export class MatchmakingGateway {
   constructor(
+    private readonly authService: AuthService,
     private readonly logger: Logger,
     private readonly matchmakingService: MatchmakingService,
     private readonly usersService: UsersService,
   ) {}
+
+  async handleConnection(client: Socket): Promise<void> {
+    if (!client.handshake.auth.token)
+      throw new UnauthorizedException('No Authorization header found');
+    const currentUser = this.authService.verifyUserFromToken(
+      client.handshake.auth.token,
+    );
+    if (!currentUser) throw new UnauthorizedException('Invalid token');
+    client.data = { id: currentUser.sub, name: currentUser.name };
+  }
 
   @SubscribeMessage('join_queue')
   async joinQueue(
--- apps/api/src/matchmaking/matchmaking.module.ts
+++ apps/api/src/matchmaking/matchmaking.module.ts
@@ -1,10 +1,11 @@
 import { Logger, Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { UsersModule } from 'src/users/users.module';
 import { MatchmakingGateway } from './matchmaking.gateway';
 import { MatchmakingService } from './matchmaking.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [AuthModule, UsersModule],
   providers: [Logger, MatchmakingService, MatchmakingGateway],
 })
 export class MatchmakingModule {}
