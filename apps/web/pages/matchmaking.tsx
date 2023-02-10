3b44b77f3bfab1553234c9ed8268bcef1a5fe3da
vfurmane <vfurmane@student.42.fr>
2023-02-10 15:09:08 +0100

    feat(web): prompt user for reconnection to the queue

--- apps/web/pages/matchmaking.tsx
+++ apps/web/pages/matchmaking.tsx
@@ -1,20 +1,30 @@
-import { ReactElement, useEffect } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import styles from "styles/matchmaking-page.module.scss";
+import { QueueReconnectionPrompt } from "../components/QueueReconnectionPrompt";
 import { useWebsocketContext } from "../components/Websocket";
 
 export default function Matchmaking(): ReactElement {
+  const [hasLeftQueue, setHasLeftQueue] = useState(false);
   const websockets = useWebsocketContext();
 
   useEffect(() => {
     return () => {
       if (websockets.matchmaking) {
         websockets.matchmaking.emit("leave_queue");
+        setHasLeftQueue(true);
       }
     };
   }, [websockets.matchmaking]);
   return (
     <div className={styles.container}>
       <h1>Waiting for an opponent...</h1>
+      {hasLeftQueue ? (
+        <QueueReconnectionPrompt
+          onReconnection={(): void => {
+            setHasLeftQueue(false);
+          }}
+        />
+      ) : null}
     </div>
   );
 }
