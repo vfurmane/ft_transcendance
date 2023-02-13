35e859dc2fa5d446de0c421a737d16377c2ee56c
sasaicic <sasaicic@student.42.fr>
2023-02-13 15:52:20 +0100

    feat(api): get, update and delete profile pictures

--- packages/types/src/entities/index.ts
+++ packages/types/src/entities/index.ts
@@ -7,6 +7,5 @@ export * from './user.entity';
 export * from './frienships.entity';
 export * from './match.entity';
 export * from './jwt.entity'
-
-
-
+export * from './upload.entity'
+export * from './profile.entity'
