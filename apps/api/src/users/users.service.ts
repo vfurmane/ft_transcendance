9b2bdc35346c5da2b55968ffda4fa3b648999cb0
sasaicic <sasaicic@student.42.fr>
2023-02-13 15:52:20 +0100

    feat(api): get, update and delete profile pictures

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,16 +1,20 @@
 import {
   BadRequestException,
-  forwardRef,
-  Inject,
   Injectable,
+  Logger,
   NotFoundException,
+  StreamableFile,
 } from '@nestjs/common';
-import { In, Repository, UpdateResult } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User, Userfront } from 'types';
+import { User, Userfront, Upload, Profile } from 'types';
 import * as speakeasy from 'speakeasy';
 import { SpeakeasyGeneratedSecretDto } from '../auth/speakeasy-generated-secret.dto';
 import { TransformUserService } from 'src/TransformUser/TransformUser.service';
+import * as fs from 'fs';
+import path from 'path';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 export interface AddUserData {
   name: string;
@@ -23,7 +27,13 @@ export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    @InjectRepository(Upload)
+    private readonly uploadRepository: Repository<Upload>,
+    @InjectRepository(Profile)
+    private readonly profileRepository: Repository<Profile>,
     private readonly transformUserService: TransformUserService,
+    private readonly httpService: HttpService,
+    private readonly logger: Logger,
   ) {}
 
   async getById(id: string): Promise<User | null> {
@@ -57,10 +67,13 @@ export class UsersService {
   }
 
   async addUser(user: AddUserData): Promise<User> {
+    const userProfile = new Profile();
+    this.profileRepository.save(userProfile);
     const userEntity = new User();
     userEntity.email = user.email;
     userEntity.name = user.name;
     if (user.password) userEntity.password = user.password;
+    userEntity.profile = userProfile;
     return this.usersRepository.save(userEntity);
   }
 
@@ -114,5 +127,89 @@ export class UsersService {
     if (usernameTaken)
       throw new BadRequestException('`username` is already in use');
     return this.usersRepository.update({ id: user.id }, { name: new_username });
+  }
+  async getProfilePicture(userId: string): Promise<StreamableFile> {
+    const userProfile = await this.profileRepository
+      .findOne({
+        where: { user: { id: userId } },
+        relations: ['user'],
+      })
+      .catch(() => {
+        throw new BadRequestException(
+          `User: ${userId} does not match any profile`,
+        );
+      });
+    if (userProfile?.picture == null) throw new NotFoundException();
+    const file = fs.createReadStream(userProfile?.picture);
+    return new StreamableFile(file);
+  }
+
+  async fetchAndStoreProfilePicture(
+    user: User,
+    profilePictureUrl: string,
+  ): Promise<void> {
+    const response = await firstValueFrom(
+      this.httpService.get(profilePictureUrl, {
+        responseType: 'stream',
+      }),
+    );
+    const filepath = `./uploads/profile_pictures/${user.id}${
+      path.parse(profilePictureUrl).ext
+    }`;
+    response.data.pipe(fs.createWriteStream(filepath));
+    this.profileRepository
+      .findOne({
+        where: { user: { id: user.id } },
+        relations: ['user'],
+      })
+      .then((profile) => {
+        this.profileRepository.update(
+          { id: profile?.id },
+          { picture: filepath },
+        );
+      });
+  }
+
+  async updateProfilePicture(
+    userId: string,
+    file: Express.Multer.File,
+  ): Promise<void> {
+    this.profileRepository
+      .findOne({
+        where: { user: { id: userId } },
+        relations: ['user'],
+      })
+      .then((profile) => {
+        if (profile?.picture != null) {
+          fs.unlink(profile?.picture, (err) => {
+            console.error(err);
+          });
+        }
+        this.profileRepository.update(
+          { id: profile?.id },
+          { picture: file.path },
+        );
+      });
+  }
+
+  async deleteProfilePicture(userId: string): Promise<void> {
+    this.profileRepository
+      .findOne({
+        where: { user: { id: userId } },
+        relations: ['user'],
+      })
+      .then((profile) => {
+        if (profile?.picture != null) {
+          fs.unlink(profile?.picture, (err) => {
+            this.logger.error(err);
+          });
+        }
+        this.profileRepository.update({ id: profile?.id }, { picture: null });
+      })
+      .catch(() => {
+        throw new BadRequestException(
+          `User: ${userId} does not match any profile`,
+        );
+      });
   }
 }
