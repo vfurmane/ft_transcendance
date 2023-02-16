61066e380d2cb1581e9f951bb9121ddb753a6261
eudier.maxence@icloud.com <eudier.maxence@icloud.com>
2023-02-16 13:49:37 +0100

    achivements in progress

--- apps/api/src/Achievements/Achievements.service.ts
+++ apps/api/src/Achievements/Achievements.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { User, Achievements } from 'types';
+import { User, Achievements, Userfront, MatchFront } from 'types';
 
 @Injectable()
 export class AchievementsService {
@@ -24,6 +24,12 @@ export class AchievementsService {
         logo: "HelloWorld"
     });
 
+    this.achivementsList.push(  {
+      title: "YIIIHA !!",
+      description: "You win your first play !",
+      logo: "Yiiiha"
+    });
+
     const titles = ['caporal', 'sergent', 'major', 'lieutenant', 'general']
     for (let i = 0; i < 5; i++)
     {
@@ -35,8 +41,38 @@ export class AchievementsService {
     }
   };
 
-  saveAchievement(winner_id: string, looser_id: string){
+  async getAchivements(user_id : string)
+  {
+    const res =  await this.userRepository.findOne({
+      relations: {
+        achievements: true, 
+      },
+      where: {
+        id: user_id,
+      },
+    });
+    if (!res)
+      throw new NotFoundException('user not found');
+    return res.achievements;
+  }
+
+  async saveAchievement(winner : User, looser : User, winnerHistory: MatchFront[], looserHistory: MatchFront[]){
     
+    const winnerAchievements = await this.getAchivements(winner.id);
+
+    console.log("winner : ", winnerHistory);
+    console.log("looser : ", looserHistory);
+    console.log("winner achivement : ", winnerAchievements);
+
+    if (winnerAchievements.length === 0)
+    {
+      const newAchievements = new Achievements();
+      newAchievements.title = this.achivementsList[0].title;
+      newAchievements.description = this.achivementsList[0].description;
+      newAchievements.user = winner;
+      newAchievements.logo = this.achivementsList[0].logo;
+      await this.AchievementsRepository.save(newAchievements);
+    }
   };
 
 }
--- apps/api/src/pong/game.service.ts
+++ apps/api/src/pong/game.service.ts
@@ -22,7 +22,7 @@ export class Game {
   public lastUpdate = 0;
   public await = true;
   public color: string[] = ['blue', 'red', 'orange', 'white', 'pink', 'black'];
-  public live = 1;
+  public live = 10;
 
   constructor(playerNumber: number, broadcaster: any) {
     this.boardType = playerNumber;
