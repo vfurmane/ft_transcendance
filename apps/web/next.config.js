2637afe32c1e5cdf21ca7cd0fa62cc4be9c38634
vfurmane <vfurmane@student.42.fr>
2023-02-18 11:13:11 +0100

    fix(web): build without typeorm in types package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,14 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: false,
   experimental: {
     transpilePackages: ["ui"],
   },
+  webpack: (config) => {
+    // Solving the following issue:
+    // https://github.com/typeorm/typeorm/issues/2841
+    config.resolve.alias.typeorm = path.resolve(__dirname, "../../node_modules/typeorm/typeorm-model-shim");
+    return config;
+  }
 };
