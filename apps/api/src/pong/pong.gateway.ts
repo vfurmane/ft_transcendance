00e07cbbf227e916a08b7f5c2f647a349c08040f
tnaton <74002970+deblokc@users.noreply.github.com>
2023-02-22 21:59:05 +0100

    prettier

--- apps/api/src/pong/pong.gateway.ts
+++ apps/api/src/pong/pong.gateway.ts
@@ -44,7 +44,6 @@ import { eventNames, listeners } from 'process';
 import { SpiedUserDto } from '../spied-user.dto';
 import { ConversationsService } from '../conversations/conversations.service';
 
-
 @UsePipes(new ValidationPipe())
 @UseInterceptors(ClassSerializerInterceptor)
 @WebSocketGateway({ namespace: 'pong' })
