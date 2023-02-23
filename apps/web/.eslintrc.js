ef450c337b5c486a3c655afb0cddde4064387be3
tnaton <74002970+deblokc@users.noreply.github.com>
2023-02-23 05:11:06 +0100

    removed eslint

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,13 +1,9 @@
 module.exports = {
-  extends: ["next", "custom"],
+  extends: [],
   parserOptions: {
     project: "tsconfig.json",
     tsconfigRootDir: __dirname,
     sourceType: "module",
   },
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-    "react/no-unescaped-entities": 0,
-  },
+  rules: {},
 };
