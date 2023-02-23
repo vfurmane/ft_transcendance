88eb7cbc1355a92d0fcde595ac563cbdaec71d34
vfurmane <vfurmane@student.42.fr>
2023-02-23 08:16:30 +0100

    fix(api): protect change username route

--- apps/api/src/users/change-name.dto.ts
+++ apps/api/src/users/change-name.dto.ts
@@ -1,7 +1,11 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import sanitize from 'sanitize-html';
 
 export class ChangeNameDto {
   @IsNotEmpty()
   @IsString()
+  @MaxLength(30)
+  @Transform(({ value }) => sanitize(value))
   new_username!: string;
 }
